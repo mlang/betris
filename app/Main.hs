b7ac6ff3aa670c727f0480a7164f72b49b0f0fb5
Mario Lang <mlang@delysid.org>
2018-08-31 01:21:06 +0200

    clearer naming

--- app/Main.hs
+++ app/Main.hs
@@ -41,20 +41,20 @@ main = do
   shutdown vty
 
 consume vty chan game = if isGameOver game then pure () else do
-  update vty $ picForImage $ string defAttr (gstr game) <|> string defAttr (show $ game ^. score)
+  update vty $ picForImage $
+   string defAttr (emboss game) <|> string defAttr (show $ game ^. score)
   e <- atomically $ readTBChan chan
   case e of
-    Ev (EvKey KEsc []) -> return ()
+    Ev (EvKey KEsc []) -> pure ()
     Tick -> timeStep game >>= consume vty chan
     Ev (EvKey KLeft []) -> consume vty chan (hardDrop game)
     Ev (EvKey KUp []) -> consume vty chan (shift Left game)
     Ev (EvKey KDown []) -> consume vty chan (shift Right game)
     Ev (EvKey KEnter []) -> consume vty chan (rotate game)
-    _ -> do
-      consume vty chan game
+    _ -> consume vty chan game
 
-gstr :: Game -> String
-gstr g = map go [1, 3 .. boardHeight] where
+emboss :: Game -> String
+emboss g = map go [1, 3 .. boardHeight] where
   go y = chr $ foldr (f y) 0x2800 [((0,0),1), ((1,0), 2), ((2,0), 4)
                                   ,((0,1),8), ((1,1),16), ((2,1),32)
                                   ,((3,0),64),((3,1),128)]
