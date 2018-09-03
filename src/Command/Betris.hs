36dd07dd316b4486ccf556af34cdb6e5d07c1e9c
Mario Lang <mlang@delysid.org>
2018-09-03 21:52:43 +0200

    IORef

--- src/Command/Betris.hs
+++ src/Command/Betris.hs
@@ -5,18 +5,19 @@
 module Command.Betris (command, Options(..), betris) where
 
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Concurrent.STM.TVar
 import Control.Concurrent.STM.TBChan
 import Control.Lens hiding (argument)
 import Control.Monad (forever)
 import Control.Monad.STM (atomically)
 import Data.Char (chr)
 import Data.Foldable (for_)
+import Data.IORef
 import qualified Data.Map as Map
 import Data.Time.Units
 import Data.Version (showVersion)
 import Game.Tetris
-import Graphics.Vty
+import Graphics.Vty hiding (Event)
+import qualified Graphics.Vty as Vty (Event)
 import Linear.V2 (V2(..))
 import Paths_betris (version)
 import Prelude hiding (Left, Right)
@@ -28,47 +29,42 @@ command = betris <$> (versionOption <*> programOptions)
 data Options = Options { initialDelay :: Millisecond } deriving (Eq, Show)
 
 programOptions :: Parser Options
-programOptions =
-  Options <$> option auto (long "initial-delay" <> metavar "DURATION" <>
-                           value (fromMicroseconds 1000000) <> showDefault <>
-                           help "Initial delay")
+programOptions = Options <$> initialDelayOption
 
 data Event e = Tick | Ev e deriving (Eq, Read, Show, Functor)
 
+betris :: Options -> IO ()
 betris Options{..} = do
   vty <- mkVty =<< userConfig
   chan <- newTBChanIO 10
   game <- initGame 0
-  speed <- newTVarIO $ fromIntegral $ toMicroseconds initialDelay
+  speed <- newIORef $ fromIntegral $ toMicroseconds initialDelay
 
+  forkIO $ forever $ nextEvent vty >>= atomically . writeTBChan chan . Ev
   forkIO $ forever $ do
-    e <- nextEvent vty
-    atomically $ writeTBChan chan $ Ev e
-
-  forkIO $ forever $ do
+    readIORef speed >>= threadDelay
+    modifyIORef speed ((-) 500)
     atomically $ writeTBChan chan Tick
-    delay <- readTVarIO speed
-    atomically $ modifyTVar speed ((-) 500)
-    threadDelay delay
 
   _ <- play vty chan game
   shutdown vty
   putStrLn ""
 
-play vty chan game
-  | isGameOver game = pure $ game ^. score
+play :: Vty -> TBChan (Event Vty.Event) -> Game -> IO Int
+play vty chan tetris
+  | isGameOver tetris = pure $ tetris ^. score
   | otherwise = do
     update vty $ picForImage $
-      string defAttr (emboss game) <|> string defAttr (show $ game ^. score)
+      string defAttr (emboss tetris) <|> string defAttr (show $ tetris ^. score)
     e <- atomically $ readTBChan chan
     case e of
-      Ev (EvKey KEsc []) -> pure $ game ^. score
-      Tick -> timeStep game >>= play vty chan
-      Ev (EvKey KLeft []) -> play vty chan (hardDrop game)
-      Ev (EvKey KUp []) -> play vty chan (shift Left game)
-      Ev (EvKey KDown []) -> play vty chan (shift Right game)
-      Ev (EvKey KEnter []) -> play vty chan (rotate game)
-      _ -> play vty chan game
+      Tick                 -> play vty chan =<< timeStep tetris
+      Ev (EvKey KLeft [])  -> play vty chan $ hardDrop tetris
+      Ev (EvKey KUp [])    -> play vty chan $ Left `shift` tetris
+      Ev (EvKey KDown [])  -> play vty chan $ Right `shift` tetris
+      Ev (EvKey KEnter []) -> play vty chan $ rotate tetris
+      Ev (EvKey KEsc [])   -> pure $ tetris ^. score
+      _                    -> play vty chan tetris
 
 emboss :: Game -> String
 emboss g = map go [1, 3 .. boardHeight] where
@@ -81,6 +77,15 @@ emboss g = map go [1, 3 .. boardHeight] where
   x = minimum $ (boardWidth - 3) : map (\(V2 x _) -> x) (coords $ g ^. block)
   fullBoard = g ^. board <> blk (g ^. block)
   blk b = Map.fromList $ map (, b ^. shape) $ coords b
+
+initialDelayOption :: Parser Millisecond
+initialDelayOption = option auto $
+    long "initial-delay"
+ <> short 'i'
+ <> metavar "DURATION"
+ <> value (fromMicroseconds 1000000)
+ <> showDefault
+ <> help "Initial delay"
 
 versionOption :: Parser (a -> a)
 versionOption = infoOption (showVersion version) $
